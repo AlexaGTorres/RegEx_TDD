caeee9ad4c748a7380964bd2b3fac9de2deee20f
AlexaGTorres <alexa.torres122694@gmail.com>
2022-11-28 21:09:05 -0800

    Checking if they are numbers and a certain amount of numbers.

--- Test/AccountTest.java
+++ Test/AccountTest.java
@@ -8,14 +8,14 @@ public class AccountTest
     @Test
     void getUsername()
     {
-        Account alexa = new Account("Alexa", "17", 448395);
+        Account alexa = new Account("Alexa", "17", "448395");
         assertTrue(alexa.getUsername().matches("Alexa"));
     }//Regular check
 
     @Test
     void setUsernameSpace()
     {
-        Account alexa = new Account("Alexa", "17", 448395);
+        Account alexa = new Account("Alexa", "17", "448395");
         alexa.setUsername(" ");
         assertTrue (alexa.getUsername().matches(" "));
     }//Checks if there is a space
@@ -23,7 +23,7 @@ public class AccountTest
     @Test
     void setUsernameNoSpace()
     {
-        Account alexa = new Account("Alexa", "17", 448395);
+        Account alexa = new Account("Alexa", "17", "448395");
         alexa.setUsername("Ori");
         assertTrue(alexa.getUsername().matches("Please Check Requirements and Try Again"));
     }//Checks when there is no space
@@ -32,7 +32,47 @@ public class AccountTest
     @Test
     void getAge()
     {
-        Account alexa = new Account("Alexa", "17", 448395);
+        Account alexa = new Account("Alexa", "17", "448395");
         assertTrue(alexa.getAge().matches("17"));
     }//Regular check
+
+    @Test
+    void setAgeNotNumber()
+    {
+        Account alexa = new Account("Alexa", "17", "448395");
+        alexa.setAge("hello");
+        assertTrue(alexa.getAge().matches("Please Check Requirements and Try Again"));
+    }
+
+    @Test
+    void setAgeNumber()
+    {
+        Account alexa = new Account("Alexa", "17", "448395");
+        alexa.setAge("16");
+        assertTrue(alexa.getAge().matches("16"));
+    }
+
+    //Id 3 tests
+    @Test
+    void getId()
+    {
+        Account alexa = new Account("Alexa", "17", "448395");
+        assertTrue(alexa.getId().matches("448395"));
+    }//Regular check
+
+    @Test
+    void setIdNotNumber()
+    {
+        Account alexa = new Account("Alexa", "17", "448395");
+        alexa.setAge("bye");
+        assertTrue(alexa.getAge().matches("Please Check Requirements and Try Again"));
+    }
+
+    @Test
+    void setIdNumber()
+    {
+        Account alexa = new Account("Alexa", "17", "448395");
+        alexa.setId("228179");
+        assertTrue(alexa.getId().matches("228179"));
+    }
 }
--- src/Account.java
+++ src/Account.java
@@ -2,9 +2,9 @@ public class Account
 {
     private String username;
     private String age;
-    private int id;
+    private String id;
 
-    public Account (String username, String age, int id)
+    public Account (String username, String age, String id)
     {
         this.username = username;
         this.age = age;
@@ -35,13 +35,30 @@ public class Account
 
     public void setAge(String age)
     {
-        if (age.matches("[0-9]"))//check for space between words
+        if (age.matches("[0-9]{2}"))//check for space between words
         {
             this.age = age;
         }
         else
         {
             this.age = "Please Check Requirements and Try Again";
+        }
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        if (id.matches("[0-9]{6}"))//check for space between words
+        {
+            this.id = id;
+        }
+        else
+        {
+            this.id = "Please Check Requirements and Try Again";
         }
     }
 }
